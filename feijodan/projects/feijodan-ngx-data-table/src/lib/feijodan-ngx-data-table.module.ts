0c6d9eaac03f42f857bdab517da75975a293226d
Daniel Feijo <feijodan@gmail.com>
2022-06-02 20:09:43 -0400

    setup the workspace and library

---
+++ feijodan/projects/feijodan-ngx-data-table/src/lib/feijodan-ngx-data-table.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { FeijodanNgxDataTableComponent } from './feijodan-ngx-data-table.component';
+
+
+
+@NgModule({
+  declarations: [
+    FeijodanNgxDataTableComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    FeijodanNgxDataTableComponent
+  ]
+})
+export class FeijodanNgxDataTableModule { }
---
+++ feijodan/projects/feijodan-ngx-data-table/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of feijodan-ngx-data-table
+ */
+
+export * from './lib/feijodan-ngx-data-table.module';
